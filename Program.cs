4cfc3a0c10593b4e230bf6387f2f2083737eaf2d
blarpet <axejob0505@gmail.com>
2022-05-15 16:05:07 +0200

    balls

--- Program.cs
+++ Program.cs
@@ -15,7 +15,6 @@ namespace WEBAPI
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
